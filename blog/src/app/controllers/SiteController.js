750c0593366cc973e4ae274441192c43274635bc
nguyengialong <sadcloudy8@gmail.com>
2023-11-18 21:18:47 +0700

    commit code

--- blog/src/app/controllers/SiteController.js
+++ blog/src/app/controllers/SiteController.js
@@ -1,8 +1,8 @@
-          class SiteController {
+            class SiteController {
     // [GET] /home
     index(req, res) {
         res.render('home');
     }
 }
 
-module.exports = new SiteController;
+module.exports = new SiteController();
